f1b894f8379cf4981dd4f520efef7413ec37178b
Satlykovs <satlykovs@gmail.com>
2025-04-11 15:04:50 +0300

    chore: small logic changes

--- Server/ProfileService/sql/init-scripts/01_create_table_user_profile.sql
+++ Server/ProfileService/sql/init-scripts/01_create_table_user_profile.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS user_profile (
     first_name TEXT DEFAULT '',
     last_name TEXT DEFAULT '',
     username TEXT DEFAULT '',
-    avatar_url TEXT DEFAULT 'https://storage.yandexcloud.net/bridge-bucket/avatars/default.jpg'
+    avatar_url TEXT DEFAULT 'https://storage.yandexcloud.net/bridge-bucket/avatars/default.jpg',
+    is_first_login BOOLEAN DEFAULT TRUE
 );
